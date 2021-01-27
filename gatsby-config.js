8ca87a30d75ce649c23b3d0e55b173126cd5534e
Kat.Chou <chouyuning@gmail.com>
2021-01-27 22:05:00 +0800

    add favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,7 +35,7 @@ module.exports = {
         background_color: `#f7f0eb`,
         theme_color: `#a2466c`,
         display: `standalone`,
-        icon: `/static/favicon.ico`
+        icon: `/favicon.ico`
       },
     },
   ],
